d8724e9cb96fd5e11a4be4e58bb92ff49785a205
dubipy <dubipi96@gmail.com>
2022-03-03 21:50:11 +0900

    LoginFormHidden

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginForm);
+    const username = loginInput.value;
+    loginForm.classList.add("hidden");
+    console.log(username)
 }
-
 loginForm.addEventListener("submit", onLoginSubmit);
-
-
-
