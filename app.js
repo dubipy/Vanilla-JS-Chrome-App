87aa81bff4e6c485d083d7818e67049db56a42b1
dubipy <dubipi96@gmail.com>
2022-03-03 22:53:54 +0900

    LoginFormSaveUsername

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    greeting.innerText = `반갑습니다. ${username}님 오늘 하루는 어떠셨나요?`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
-loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
+  greeting.innerText = `반갑습니다. ${username}님 오늘 하루는 어떠셨나요?`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
